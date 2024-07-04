eca7b8e8a04268334dd7add0fe37b322dd288f5d
Elijah <eachala5@gmail.com>
2024-07-04 21:55:47 +0000

    5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,8 +4,10 @@
     Basic annotations for variables.
 '''
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     '''
         Returns sum of the floats in the list.
     '''
