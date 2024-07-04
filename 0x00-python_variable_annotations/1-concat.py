544fdee491c64a90381b7631b73fb1685e634c13
Elijah <eachala5@gmail.com>
2024-07-04 21:23:21 +0000

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        Returns concatenation of the two strings.
+    '''
+    return str1 + str2
